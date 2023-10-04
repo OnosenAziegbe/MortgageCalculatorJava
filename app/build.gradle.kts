cf0de87a9b36553643c929368d48982f4344f598
Onosen Aziegbe <onosen11@gmail.com>
2023-10-04 16:32:03 -0400

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mortgagecalculatorjava"
-    compileSdk = 33
+    namespace = "com.example.noteme"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.mortgagecalculatorjava"
+        applicationId = "com.example.noteme"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -41,4 +40,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Kotlin stdlib dependencies with explanatory comments
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") // kotlin-stdlib-jdk7 is now a part of kotlin-stdlib
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") // kotlin-stdlib-jdk8 is now a part of kotlin-stdlib
 }
